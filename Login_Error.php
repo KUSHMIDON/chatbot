4aa1646d4e9c482bbcef55eb7fdefb28e6ab06f0
KUMAR SHUBHAM <37311423+KUSHMIDON@users.noreply.github.com>
2018-06-22 13:58:22 +0200

    Add files via upload

---
+++ Login_Error.php
@@ -0,0 +1,11 @@
+<html>
+   
+   <head>
+      <title>Login Error </title>
+   </head>
+   
+   <body align='center'>
+      <h1><?php echo $name ?> You are not logged in. Please login again</h1>
+   </body>
+   
+</html>
---
+++ ngnlogout.php
@@ -0,0 +1,12 @@
+<html>
+   
+   <head>
+      <title>Logout </title>
+   </head>
+   
+   <body align='center'>
+      <h1>You are successfully logged out. Have a nice day !</h1>
+	  
+   </body>
+   
+</html>
